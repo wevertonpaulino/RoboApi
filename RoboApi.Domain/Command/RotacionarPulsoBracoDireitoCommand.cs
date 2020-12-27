12b3d8d50956b342fa99750316cc7d924de2ba80
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-27 10:28:59 -0300

    Implementa classes command

---
+++ RoboApi.Domain/Command/RotacionarPulsoBracoDireitoCommand.cs
@@ -0,0 +1,13 @@
+﻿using RoboApi.Domain.Facade;
+
+namespace RoboApi.Domain.Command
+{
+    public class RotacionarPulsoBracoDireitoCommand : RoboCommand
+    {
+        public int Estado { get; set; }
+
+        public RotacionarPulsoBracoDireitoCommand(Robo robo) : base(robo) { }
+
+        public override void Executar() => _robo.RotacionarPulsoBracoDireito(Estado);
+    }
+}
