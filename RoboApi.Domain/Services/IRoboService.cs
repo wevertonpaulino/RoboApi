4772f1d64a8a1936b03a901fbcb549f27ed6e753
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-27 11:03:01 -0300

    Implementa o serviço

---
+++ RoboApi.Domain/Services/IRoboService.cs
@@ -0,0 +1,13 @@
+﻿namespace RoboApi.Domain.Services
+{
+    public interface IRoboService
+    {
+        string EstadoAtual();
+        void RotacionarCabeca(int estado);
+        void InclinarCabeca(int estado);
+        void MoverCotoveloBracoDireito(int estado);
+        void MoverCotoveloBracoEsquerdo(int estado);
+        void RotacionarPulsoBracoDireito(int estado);
+        void RotacionarPulsoBracoEsquerdo(int estado);
+    }
+}
---
+++ RoboApi.Domain/Services/RoboService.cs
@@ -0,0 +1,24 @@
+﻿using RoboApi.Domain.Command;
+using RoboApi.Domain.Facade;
+
+namespace RoboApi.Domain.Services
+{
+    public class RoboService : IRoboService
+    {
+        private readonly Robo _robo = new Robo();
+
+        public string EstadoAtual() => _robo.EstadoAtual();
+
+        public void RotacionarCabeca(int estado) => new RotacionarCabecaCommand(_robo) { Estado = estado }.Executar();
+
+        public void InclinarCabeca(int estado) => new InclinarCabecaCommand(_robo) { Estado = estado }.Executar();
+
+        public void MoverCotoveloBracoDireito(int estado) => new MoverCotoveloBracoDireitoCommand(_robo) { Estado = estado }.Executar();
+
+        public void MoverCotoveloBracoEsquerdo(int estado) => new MoverCotoveloBracoEsquerdoCommand(_robo) { Estado = estado }.Executar();
+
+        public void RotacionarPulsoBracoDireito(int estado) => new RotacionarPulsoBracoDireitoCommand(_robo) { Estado = estado }.Executar();
+
+        public void RotacionarPulsoBracoEsquerdo(int estado) => new RotacionarPulsoBracoEsquerdoCommand(_robo) { Estado = estado }.Executar();
+    }
+}
